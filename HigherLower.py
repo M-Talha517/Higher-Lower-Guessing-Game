57fc5daadff1381fb9640d10086432679fe2949e
Muhammad Tahla <54750781+M-Talha517@users.noreply.github.com>
2021-04-03 16:13:10 +0500

    Create HigherLower.py

---
+++ HigherLower.py
@@ -0,0 +1,47 @@
+from game_data import data
+from art import logo,vs
+from random import choice
+from replit import clear
+
+def getRandomData(data):
+  statement = choice(data)
+  str = f"{statement['name']}, a {statement['description']}, from {statement['country']}"
+  return str,statement['follower_count']
+
+def printData(statementA,statementB):
+  
+  print(logo)
+  print('Compare A: '+statementA)
+  print(vs)
+  print('Against B: '+statementB)
+
+def main():
+  score = 0 
+  statementB = getRandomData(data) 
+  continue_guessing = True
+
+  while continue_guessing:
+    statementA = statementB
+    while statementA == statementB:
+      statementB = getRandomData(data) 
+
+    printData(statementA[0],statementB[0])
+    user_guess = input("Which Do You Think has More Follower Count. 'A' or 'B' : ").lower()
+    if checkResult(statementA[1],statementB[1],user_guess):
+      clear()
+      score += 1;
+      print(f'Correct Guess .Your Score is {score}')
+    else:
+      continue_guessing = False
+  
+  print(f"GAME OVER : YOUR FINAL SCORE IS : {score}")
+  
+def checkResult(first,second,guess):
+    if first>second:
+      return guess == 'a'
+    else:
+      return guess == 'b'   
+
+
+main()
+
